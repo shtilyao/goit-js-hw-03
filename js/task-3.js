a53b9fe8879a1ee5ddc921070565bd83609c4730
Shtill <shtilyao@gmail.com>
2026-05-31 13:41:29 +0300

    JS HW 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,17 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  if (lowerCaseMessage.includes("sale") || lowerCaseMessage.includes("spam")) {
-    return true;
-  } else {
-    return false;
-  }
+function filterArray(numbers, value) {
+const arr = [];
+
+for (const number of numbers) {
+if (number > value) {
+arr.push(number);
+}
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
